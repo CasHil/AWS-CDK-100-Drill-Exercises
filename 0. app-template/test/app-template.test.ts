b976d29bf3c13ecee03744c50f49840c10c3cae1
Casper Hildebrand <casperhildebrand@gmail.com>
2026-02-24 17:31:52 +0100

    Create assignment template

---
+++ 0. app-template/test/app-template.test.ts
@@ -0,0 +1,17 @@
+import { test } from "@jest/globals";
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppTemplate from '../lib/app-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/app-template-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new AppTemplate.AppTemplateStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
