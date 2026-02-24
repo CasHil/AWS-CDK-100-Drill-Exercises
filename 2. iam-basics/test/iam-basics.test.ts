d04fa119b5d6007e42001bb7cdd10592532abc17
Casper Hildebrand <casperhildebrand@gmail.com>
2026-02-24 17:33:28 +0100

    Set up assignment 2 for IAM basics

---
+++ 2. iam-basics/test/iam-basics.test.ts
@@ -0,0 +1,17 @@
+﻿import { test } from "@jest/globals";
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IamBasics from '../lib/iam-basics-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iam-basics-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new IamBasics.IamBasicsStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
