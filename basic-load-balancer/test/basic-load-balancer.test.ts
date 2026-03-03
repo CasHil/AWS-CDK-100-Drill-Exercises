e77d9c7c70c9427d913194bfdae1c7f28f01ac6e
Casper Hildebrand <casperhildebrand@gmail.com>
2026-03-03 10:29:05 +0100

    Test basic load balancer more extensively

--- basic-load-balancer/test/basic-load-balancer.test.ts
+++ basic-load-balancer/test/basic-load-balancer.test.ts
@@ -1,23 +1,43 @@
 import * as cdk from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
 import * as BasicLoadBalancer from "../lib/basic-load-balancer-stack";
 
-test("Infrastructure Created", () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new BasicLoadBalancer.BasicLoadBalancerStack(
-    app,
-    "MyTestStack",
-  );
-  // THEN
-  const template = Template.fromStack(stack);
+const app = new cdk.App();
+const stack = new BasicLoadBalancer.BasicLoadBalancerStack(app, "MyTestStack");
+const template = Template.fromStack(stack);
 
+test("Load balancer is internet-facing", () => {
   template.hasResourceProperties("AWS::ElasticLoadBalancingV2::LoadBalancer", {
     Scheme: "internet-facing",
   });
+});
 
+test("Auto Scaling Group is configured with correct capacity limits", () => {
   template.hasResourceProperties("AWS::AutoScaling::AutoScalingGroup", {
     MinSize: "1",
     MaxSize: "4",
+  });
+
+  template.hasResourceProperties("AWS::AutoScaling::ScalingPolicy", {
+    PolicyType: "TargetTrackingScaling",
+    TargetTrackingConfiguration: {
+      PredefinedMetricSpecification: {
+        PredefinedMetricType: "ASGAverageCPUUtilization",
+      },
+      TargetValue: 50,
+    },
+  });
+});
+
+test("ALB Security Group allows HTTP traffic from anywhere", () => {
+  template.hasResourceProperties("AWS::EC2::SecurityGroup", {
+    SecurityGroupIngress: Match.arrayWith([
+      Match.objectLike({
+        CidrIp: "0.0.0.0/0",
+        FromPort: 80,
+        IpProtocol: "tcp",
+        ToPort: 80,
+      }),
+    ]),
   });
 });
